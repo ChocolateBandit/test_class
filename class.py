dfbbb26af84c2664fa4a8f288f937f46eff69af6
ChocolateBandit <kadyrkulove@mail.ru>
2023-01-23 20:26:18 +0600

    fdfd

--- class.py
+++ class.py
@@ -1,19 +1,36 @@
-class House:
-    '''Описание нашего дома'''
-    def __init__(self,street,number,year):
-        self.street = street
-        self.number = number
-        self.year = year
+# class House:
+#     '''Описание нашего дома'''
+#     def __init__(self,street,number,year):
+#         self.street = street
+#         self.number = number
+#         self.year = year
         
-    def get_information(self):
-        return f"я живу на улице {self.street} под номером {self.number}"
+#     def get_information(self):
+#         return f"я живу на улице {self.street} под номером {self.number}"
     
-    def get_year(self,year):
-        age = year + self.age
+#     def get_year(self,year):
+#         age = year + self.age
     
-House1 = House("Абдрахманова", "101/1")
-print(House1.get_information())
-House2 = House.get_year(2025)
-print()
+# House1 = House("Абдрахманова", "101/1")
+# print(House1.get_information())
+# House2 = House.get_year(2025)
+# print()
+
+class Car():
+    distance = 
+    def __init__(self,brand,model,year,speed,color):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.speed = speed
+        self.color = color
+        
+    def __str__(self):
+        return f"{self.brand} куплена по заказу. год выпуска {self.year}"
+        
+    def get_time(self,speed):
+        return self.distance / speed
+    
+car1 = Car("dodge", "viper", 2020, )
     
     
